04146f4cdbf353c99cda79361ccd7e886db375fb
AbdElrahmanSaaad <bodyeg123@gmail.com>
2024-03-30 00:42:27 +0200

    edit workflow

--- main.tf
+++ main.tf
@@ -4,13 +4,38 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "aws_security_group" "ssh_access" {
+  name        = "ssh-access"
+  description = "Allow SSH access from my IP"
 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  
+  }
+
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
 resource "aws_instance" "my-ec2"{
     ami= var.image
     instance_type= var.instance_type
     key_name      = "Abdokey"
-     
+    security_groups = [aws_security_group.ssh_access.name]
+ 
     
 }
 resource "aws_eip" "my_eip" {
