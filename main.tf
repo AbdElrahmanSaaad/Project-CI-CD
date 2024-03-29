0924fd06393b8e469704a1d202200212d6801862
AbdElrahmanSaaad <bodyeg123@gmail.com>
2024-03-29 23:32:17 +0200

    edit terraform

--- main.tf
+++ main.tf
@@ -4,39 +4,12 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-resource "aws_security_group" "ssh_access" {
-  name        = "ssh-access"
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  
-  }
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  
-  }
-
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-data "aws_key_pair" "existing_key_pair" {
-  key_name = "Abdokey"
-  
-}
 
 resource "aws_instance" "my-ec2"{
     ami= var.image
     instance_type= var.instance_type
-    key_name      = data.aws_key_pair.existing_key_pair.key_name
+    key_name      = "Abdokey"
     security_groups = [aws_security_group.ssh_access.name]
      
     
