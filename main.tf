f55367edbae15472c44a7de20100c3d8c71923b2
AbdElrahmanSaaad <bodyeg123@gmail.com>
2024-03-29 22:59:04 +0200

    initial commit

---
+++ main.tf
@@ -0,0 +1,53 @@
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_security_group" "ssh_access" {
+  name        = "ssh-access"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  
+  }
+
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+data "aws_key_pair" "existing_key_pair" {
+  key_name = "Abdokey"
+  
+}
+
+resource "aws_instance" "my-ec2"{
+    ami= var.image
+    instance_type= var.instance_type
+    key_name      = data.aws_key_pair.existing_key_pair.key_name
+    security_groups = [aws_security_group.ssh_access.name]
+     
+    
+}
+resource "aws_eip" "my_eip" {
+  vpc = true  
+}
+resource "aws_eip_association" "my_eip_assoc" {
+  instance_id   = aws_instance.my-ec2.id
+  allocation_id = aws_eip.my_eip.id
+}
+output "instance_ip" {
+  value = aws_eip.my_eip.public_ip
+}
